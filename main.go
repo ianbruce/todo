1d9078cf78cff88c44e6690d796cd000fabb1e7c
Ian Bruce <brucei@u.washington.edu>
2018-06-18 15:23:27 -0700

    create dependencies in main launcher, create cache proxy to database

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/patrickmn/go-cache"
+
 	"github.com/ianbruce/todo/routers"
+	"github.com/ianbruce/todo/persistence"
+	"github.com/ianbruce/todo/injects"
+
 )
 
 func main() {
-	log.Printf("Server started")
+	myDynamo := dynamodb.New(session.New(), &aws.Config{Region: aws.String("us-west-2")})
 
-	router := routers.NewRouter()
+	dynamoTodo := persistence.DynamoTodo{
+		DB: myDynamo,
+		TableName: "todos",
+	}
+
+	cachingProxy := persistence.CacheProxy{
+		Proxied: &dynamoTodo,
+		Cache: cache.New(5*time.Minute, 10*time.Minute),
+	}
+
+	appContainer := injects.AppContainer{
+		DB: &cachingProxy,
+	}
+
+	router := routers.NewRouter(appContainer)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ persistence/cacheProxy.go
@@ -0,0 +1,46 @@
+package persistence
+
+import (
+  "github.com/patrickmn/go-cache"
+
+  "github.com/ianbruce/todo/model"
+)
+
+type CacheProxy struct {
+  Proxied TodoDatabase
+  Cache *cache.Cache
+}
+
+func (proxy *CacheProxy) GetList(id string) (model.TodoList, error) {
+  list, found := proxy.Cache.Get(id)
+
+  if found {
+    return list.(model.TodoList), nil
+  }
+
+  newList, err := proxy.Proxied.GetList(id)
+
+  if err != nil {
+    return newList, err
+  }
+
+  proxy.Cache.Set(id, newList, cache.DefaultExpiration)
+
+  return newList, nil
+}
+
+func (proxy *CacheProxy) GetLists(query string, limit int) ([]model.TodoList, error) {
+  return proxy.Proxied.GetLists(query, limit)
+}
+
+func (proxy *CacheProxy) CreateList(list model.TodoList) error {
+  return proxy.Proxied.CreateList(list)
+}
+
+func (proxy *CacheProxy) CreateTask(listId string, task model.Task) error {
+  return proxy.Proxied.CreateTask(listId, task)
+}
+
+func (proxy *CacheProxy) UpdateTaskStatus(listId string, taskId string) error {
+  return proxy.Proxied.UpdateTaskStatus(listId, taskId)
+}
