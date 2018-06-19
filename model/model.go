93caa9ecf1e00611aca1722fd3c93b7205f96949
Ian Bruce <brucei@u.washington.edu>
2018-06-19 04:26:26 -0700

    updated tags of model for json marshalling

--- model/model.go
+++ model/model.go
@@ -1,16 +1,16 @@
 package model
 
 type Task struct {
-	ID           string `dynamodbav:"taskID"`
-	Name         string `dynamodbav:"taskName"`
-	Completed    bool   `dynamodbav:"taskCompleted"`
+	ID           string `dynamodbav:"taskID", json: "id"`
+	Name         string `dynamodbav:"taskName", json: "name"`
+	Completed    bool   `dynamodbav:"taskCompleted", json: "completed"`
 }
 
 type TodoList struct {
-	ID           string `dynamodbav:"listID"`
-	Name         string `dynamodbav:"listName"`
-	Description  string `dynamodbav:"listDescription"`
-	Tasks        []Task `dynamodbav:"tasks"`
+	ID           string `dynamodbav:"listID", json: "id"`
+	Name         string `dynamodbav:"listName", json: "name"`
+	Description  string `dynamodbav:"listDescription", json: "description"`
+	Tasks        []Task `dynamodbav:"tasks", json: "tasks"`
 }
 
 type CompletedTask struct {
